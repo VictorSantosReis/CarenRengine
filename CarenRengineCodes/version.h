2147a8db6e262a4aa47aa0fb2440656687265aca
VictorSReis_PFTA <victorsantosreis@hotmail.com>
2021-02-09 15:24:52 -0300

    remoção de arquivos desnecessários.

--- CarenRengineCodes/version.h
+++ CarenRengineCodes/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 9
 #define VERSION_PATCH 0
-#define VERSION_BUILD 15
+#define VERSION_BUILD 18
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
